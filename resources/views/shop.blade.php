dfcff5469aa3416cadb7b1c421056da9275b30be
Radar3759 <chainmailgirl@gmail.com>
2023-10-31 11:55:18 -0700

    -add pagination

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.base')
 @push('styles')
-<link id="color-link" rel="stylesheet" type="text/css" href="assets/css/demo2.css">
+    <link id="color-link" rel="stylesheet" type="text/css" href="assets/css/demo2.css">
+    <style>
+        nav svg {
+            height: 20px;
+        }
+    </style>
 @endpush
 
 @section('content')
@@ -520,35 +525,7 @@
                         @endforeach
                     </div>
 
-                    <nav class="page-section">
-                        <ul class="pagination">
-                            <li class="page-item">
-                                <a class="page-link" href="javascript:void(0)" aria-label="Previous"
-                                    style="color:#6c757d;">
-                                    <span aria-hidden="true">
-                                        <i class="fas fa-chevron-left"></i>
-                                    </span>
-                                </a>
-                            </li>
-
-
-                            <li class="page-item active">
-                                <a class="page-link" href="javascript:void(0)">1</a>
-                            </li>
-                            <li class="page-item">
-                                <a class="page-link" href="shop-1.html?page=2">2</a>
-                            </li>
-
-                            <li class="page-item">
-                                <a href="shop-1.html?page=2" class="page-link" aria-label="Next">
-                                    <span aria-hidden="true">
-                                        <i class="fas fa-chevron-right"></i>
-                                    </span>
-                                </a>
-                            </li>
-
-                        </ul>
-                    </nav>
+                    {{$products->links("pagination.default")}}
 
                 </div>
             </div>
