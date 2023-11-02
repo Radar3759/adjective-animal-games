3424416f721984ca6970ecc8bd99815bea0dae8a
Radar3759 <chainmailgirl@gmail.com>
2023-11-02 11:39:42 -0700

    -update CartController routes, fix typos

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Cart;
 
 class CartController extends Controller
 {
     public function index()
     {
-        return view('cart');
+        $cartItems = Cart::instance('cart')->content();
+        return view('cart', ['cartItems'=>$cartItems]);
     }
 }
